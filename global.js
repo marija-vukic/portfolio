9a8109767fa29f4dddd02db0ed0bf2998d5ae6c8
Marija <mvukic@ucsd.edu>
2025-01-24 22:08:57 -0800

    lab 3

--- global.js
+++ global.js
@@ -3,3 +3,79 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+//PART THREE
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/marija-vukic/', title: 'Github' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+  if (a.host != location.host) {
+    a.target = "_blank"
+  }
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+const select = document.querySelector('.color-scheme select');
+select.addEventListener('input', function (event) {
+  console.log('Color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+});
+
+select.addEventListener('input', function (event) {
+  console.log('Color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+let form = document.querySelector('form');
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let url = form.action + '?';
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  url += params.join('&');
+  console.log('Generated URL:', url);
+  location.href = url;
+});
