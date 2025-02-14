ad22117c90647394c02294697ffae16b7ead54cc
Marija <mvukic@ucsd.edu>
2025-02-14 08:00:40 -0800

    test

---
+++ meta/main.js
@@ -0,0 +1,18 @@
+let data = [];
+
+async function loadData() {
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: +row.line,
+    depth: +row.depth,
+    length: +row.length,
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+  
+  console.log(data); 
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadData();
+});
